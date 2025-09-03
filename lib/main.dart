6003dad6083bffd4d864842b9f73a9444abbf30c
lenicealves945-netizen <elenicealves945@gmail.com>
2025-09-03 11:05:01 -0300

    criação de acesso para manipulação de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+
 import 'package:flutter/material.dart';
+import 'tela_home.dart';
 import 'tela_login.dart';
+import 'usuario.dart';
+import 'restaurante.dart';
+import 'tipo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,20 +12,25 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Telalogin(),
+    );
+
+
+
     /*
     Usuario u = Usuario(
       codigo: 1,
       login: "Elenice123",
       nome: "Elenice",
       senha: "@senhaforte123"
-    );*/
+    );
+    */
 
-
-    return MaterialApp(
-   debugShowCheckedModeBanner: false,
-      home: Telalogin()
-    ); //materialapp
   }
 }
